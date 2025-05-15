48a03be352d430ce0e04150291dc683aab535fe3
yamasho-me <yamasho4014@gmail.com>
2025-05-15 22:06:26 +0900

    fix Thema bags

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ class ThemeToggler {
     this.button = document.getElementById(buttonId);
     this.target = target;
     this.themeKey = "theme";
+    this.html = document.documentElement;
 
     this.applySavedTheme();
     this.button.addEventListener("click", () => this.toggleTheme());
@@ -12,13 +13,16 @@ class ThemeToggler {
     const saved = localStorage.getItem(this.themeKey);
     if (saved === "dark") {
       this.target.classList.add("dark");
+      this.html.classList.add("dark");
     } else {
       this.target.classList.remove("dark");
+      this.html.classList.remove("dark");
     }
   }
 
   toggleTheme() {
     const isDark = this.target.classList.toggle("dark");
+    this.html.classList.toggle("dark", isDark);
     localStorage.setItem(this.themeKey, isDark ? "dark" : "light");
   }
 }
